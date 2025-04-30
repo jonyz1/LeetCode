5b78ffeb0b391de4d3ed8007bf17126ba033546c
jonyz1 <108366549+jonyz1@users.noreply.github.com>
2025-04-30 14:10:27 +0300

    Time: 18 ms (12.73%), Space: 60.5 MB (93.93%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    const val = [
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1
+    ];
+    
+    const syms = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"
+    ];
+    
+    let result = '';
+    
+    for (let i = 0; i < val.length; i++) {
+        while (num >= val[i]) {
+            result += syms[i];
+            num -= val[i];
+        }
+    }
+    
+    return result;
+};
